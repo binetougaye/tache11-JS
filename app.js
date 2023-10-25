b0cef7b181596f9ae152bc629d846242e6272bac
Fatou Binetou GAYE <gayefatimabinetou@gmail.com>
2023-10-25 13:32:43 +0200

    tache10 JS

---
+++ app.js
@@ -0,0 +1,7 @@
+// function getValue() {}
+// let inputValue = document.getElementById("input");
+function getValue() {
+  let inputValue = document.getElementById("input").value;
+  alert(inputValue);
+}
+getValue();
