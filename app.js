32047f11a8156d6cce158b7ec89be4c9ec61794a
Fatou Binetou GAYE <gayefatimabinetou@gmail.com>
2023-11-13 14:08:18 +0100

    changeBg

--- app.js
+++ app.js
@@ -3,28 +3,48 @@ let container = document.querySelector(".todo");
 let input = document.getElementById("input");
 let btnAdd = document.querySelector(".btn");
 let todoBtn = document.getElementById("todo");
-console.log(todoBtn);
+// console.log(todoBtn);
 let todoDoing = document.getElementById("doing");
 let todoDone = document.getElementById("done");
 let ajout = document.getElementById("ajout");
-
+let tabTodos = [];
+// tabTodos = [
+// {
+//   title: input.value,
+//   id: Math.floor(Math.random()*100 + 1),
+// }
+// ]
 // Event Listeners
 btnAdd.addEventListener("click", function () {
-  container.innerHTML += ` <div class="row justify-content-center align-items-center">
-  <div class="col-lg-6 ajout">
-      <p class="mt-2" id="ajout">${input.value}</p>
-  </div>
-  <div class="col-lg-6 text-end buttons">
-      <button id="todo" class="border-0 text-white">To do</button>
-      <button id="doing" class="border-0 text-white">Doing</button>
-      <button id="done" class="border-0 text-white">Done</button>
-  </div>
-</div>`;
-});
+  let newTodo = {
+    title: input.value,
+    id: Math.floor(Math.random() * 100 + 1),
+  };
+  tabTodos.push(newTodo);
+  tabTodos.forEach((todo) => {
+    container.innerHTML += ` <div class="mt-2 row justify-content-center align-items-center">
+    <div class="col-lg-6 ajout">
+        <p class="mt-2 ${todo.id}" id="ajout">${todo.title}</p>
+    </div>
+    <div class="col-lg-6 text-end buttons">
+        <button id="todo" class="border-0 text-white">To do</button>
+        <button id="doing" class="border-0 text-white" onclick="changeBgOrange(this)">Doing</button>
+        <button id="done" class="border-0 text-white" onclick = "changeBgGreen(this)">Done</button>
+    </div>
+  </div>`;
+  });
 
-// todoBtn.addEventListener("click", function () {});
-// How to create new element
-// // let newElement = document.createElement("p");
-// // newElement.innerHTML = "hello";
-// let newContent = document.createTextNode("hi");
-// // console.log(newElement);
+  // container.style.backgroundColor = "red";
+});
+function changeBgOrange(e) {
+  console.log(e);
+  let rowTodo = e.parentElement.parentElement;
+  console.log(rowTodo);
+  rowTodo.classList.add("bg-warning");
+}
+function changeBgGreen(e) {
+  console.log(e);
+  let rowTodo = e.parentElement.parentElement;
+  console.log(rowTodo);
+  rowTodo.classList.add("bg-success");
+}
